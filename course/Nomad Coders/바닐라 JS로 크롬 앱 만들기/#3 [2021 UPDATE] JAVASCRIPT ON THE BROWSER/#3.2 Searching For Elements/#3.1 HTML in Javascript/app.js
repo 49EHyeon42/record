c0c2a7eaf180a97f21ca2488b69a77d99844c11e
49EHyeon42 <49EHyeon42@gmail.com>
2021-08-25 16:40:12 +0900

    Add Nomad Coders/바닐라 JS.../chapter3/3.2

---
+++ course/Nomad Coders/바닐라 JS로 크롬 앱 만들기/#3 [2021 UPDATE] JAVASCRIPT ON THE BROWSER/#3.2 Searching For Elements/#3.1 HTML in Javascript/app.js
@@ -0,0 +1,10 @@
+const hellos = document.getElementsByClassName(hello);
+console.log(hellos);
+
+const title = document.getElementsByTagName("h1");
+console.log(title);
+
+const titlee = document.querySelector(".hello h1");
+// document.querySelectorAll
+
+console.log(titlee)
