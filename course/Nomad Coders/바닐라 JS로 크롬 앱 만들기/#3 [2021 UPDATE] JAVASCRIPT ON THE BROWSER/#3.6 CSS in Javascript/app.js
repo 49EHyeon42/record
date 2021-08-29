6ce99985aee593b23d2b4ee824ac9381fce5aa16
49EHyeon42 <49EHyeon42@gmail.com>
2021-08-29 17:13:58 +0900

    Add Nomad Coders/바닐라 JS.../chapter3/3.6

---
+++ course/Nomad Coders/바닐라 JS로 크롬 앱 만들기/#3 [2021 UPDATE] JAVASCRIPT ON THE BROWSER/#3.6 CSS in Javascript/app.js
@@ -0,0 +1,23 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleMouseEnter() {
+    // if(h1.style.color === "blue") {
+    //     h1.style.color = "tomato";
+    // } else {
+    //     h1.style.color = "blue";
+    // }
+
+    const currentColor = h1.style.color;
+    let newColor;
+    if(currentColor === "blue") {
+        newColor = "tomato";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
+}
+
+h1.addEventListener("click", handleMouseEnter)
+
+/*
+*/
