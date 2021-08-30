7ad53224bf222add4e563df1cfe8895aaaad0ade
49EHyeon42 <49EHyeon42@gmail.com>
2021-08-30 09:54:45 +0900

    Add Nomad Coders/바닐라 JS.../chapter3/3.7

---
+++ course/Nomad Coders/바닐라 JS로 크롬 앱 만들기/#3 [2021 UPDATE] JAVASCRIPT ON THE BROWSER/#3.7 CSS in Javascript Part Two/app.js
@@ -0,0 +1,15 @@
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleMouseEnter() {
+    const clickedClass = "clicked"
+    if(h1.className === clickedClass) {
+        h1.className = "";
+    } else {
+        h1.className = clickedClass;
+    }
+}
+
+h1.addEventListener("click", handleMouseEnter)
+
+/*
+*/
