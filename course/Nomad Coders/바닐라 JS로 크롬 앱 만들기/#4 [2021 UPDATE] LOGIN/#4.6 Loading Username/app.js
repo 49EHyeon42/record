26ad99552b5867a43057dff67ca39cf402a33944
49EHyeon42 <49EHyeon42@gmail.com>
2021-09-01 11:56:22 +0900

    Add Nomad Coders/바닐라 JS.../chapter4/4.6

---
+++ course/Nomad Coders/바닐라 JS로 크롬 앱 만들기/#4 [2021 UPDATE] LOGIN/#4.6 Loading Username/app.js
@@ -0,0 +1,37 @@
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username"
+
+function onLoginSubmit() {
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username)
+}
+
+function paintGreetings(username) {
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if (savedUsername === null) {
+    // show the form
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+    // show the greetings
+    paintGreetings(savedUsername)
+}
+
+/*
+    saving example : 유튜브 볼륨 조절
+    local storage 로 저장
+    setItem, getItem, removeItem 으로 사용
+
+*/
