c8e640322c971adf1b47121692bd7cf569bce887
49EHyeon42 <49EHyeon42@gmail.com>
2021-09-10 17:17:32 +0900

    Study Nomad Coders/React JS.../movie_app_2019 & memo.md 3.1

--- course/Nomad Coders/ReactJS로 영화 웹 서비스 만들기/movie_app_2019/src/App.js
+++ course/Nomad Coders/ReactJS로 영화 웹 서비스 만들기/movie_app_2019/src/App.js
@@ -6,11 +6,19 @@ class App extends React.Component {
     count: 0
   };
 
+  // setState를 호출할 때마다 react는 새로운 state와 함께 render function을 호출
+  
   add = () => {
-    console.log("add");
+    // 비추천
+    // this.setState({ count: this.state.count +1});
+    // react에서 외부의 상태에 의존하지 않는 가장 좋은 방법
+    this.setState(current => ({ count: current.count + 1}));
   };
   minus = () => {
-    console.log("minus");
+    // 비추천
+    // this.setState({ count: this.state.count -1});
+    // react에서 외부의 상태에 의존하지 않는 가장 좋은 방법
+    this.setState(current => ({ count: current.count - 1}));
   };
   
   render() {
